fd7e9ccb5823f01a09250baba05aa9679d2eabbb
seravellenyravalen-hub <seravellenyravalen@gmail.com>
2026-09-03 06:56:39 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,25 +2,3 @@ plugins {
     id("com.android.application") version "8.13.0" apply false
     id("org.jetbrains.kotlin.android") version "2.2.10" apply false
 }
-
-android {
-    namespace = "com.gamevision.companion"
-    compileSdk = 36
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    defaultConfig {
-        applicationId = "com.gamevision.companion"
-        minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0.0"
-    }
-}
