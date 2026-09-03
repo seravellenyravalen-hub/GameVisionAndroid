e30feac7071e880ab796e7df5b904e24d89c41e9
seravellenyravalen-hub <seravellenyravalen@gmail.com>
2026-09-03 06:59:18 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,15 @@ android {
     namespace = "com.gamevision.companion"
     compileSdk = 36
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     defaultConfig {
         applicationId = "com.gamevision.companion"
         minSdk = 26
